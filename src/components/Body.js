fa0ae26bd7fcc270fa98cfc0f8890db9c81fda1f
Zahid <zahidmohammad495@gmai.com>
2023-11-18 12:05:15 +0530

    useState to filter and update data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import RestaurentCard from "./RestaurentCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  let [topRatedList, setTopRatedList] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filterList = topRatedList.filter(
+              (restaurent) => restaurent.info.avgRating >= 4
+            );
+            console.log(filterList);
+            setTopRatedList(filterList);
+          }}
+        >
+          Top Rated Restaurents
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurent) => (
+        {topRatedList.map((restaurent) => (
           <RestaurentCard key={restaurent.info.id} resData={restaurent} />
         ))}
       </div>
