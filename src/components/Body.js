4b20e63339dacd0fe492cb4dcf2225cf9f1e37cd
Zahid <zahidmohammad495@gmai.com>
2023-11-21 14:32:01 +0530

    add shimmer component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,41 +1,43 @@
 import RestaurentCard from "./RestaurentCard";
 import { useState, useEffect } from "react";
+import { API_URL } from "../utils/constant";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  let [topRatedList, setTopRatedList] = useState([]);
+  let [listOfRestaurents, setListOfRestaurents] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.585445&lng=73.712479&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const data = await fetch(API_URL);
     const json = await data.json();
-    console.log(json);
-    setTopRatedList(
-      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    // console.log(json);
+    setListOfRestaurents(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  return (
+  return listOfRestaurents.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            let filterList = topRatedList.filter(
-              (restaurent) => restaurent.info.avgRating >= 4
+            let filterList = listOfRestaurents.filter(
+              (restaurent) => restaurent?.info?.avgRating >= 4
             );
-            console.log(filterList);
-            setTopRatedList(filterList);
+            // console.log(filterList);
+            setListOfRestaurents(filterList);
           }}
         >
           Top Rated Restaurents
         </button>
       </div>
       <div className="res-container">
-        {topRatedList.map((restaurent) => (
+        {listOfRestaurents.map((restaurent) => (
           <RestaurentCard key={restaurent.info.id} resData={restaurent} />
         ))}
       </div>
