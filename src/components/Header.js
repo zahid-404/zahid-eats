0f179728ab43e1c6714d8477735669fa6eac97a3
Zahid <zahidmohammad495@gmai.com>
2023-12-27 11:05:16 +0530

    fix some logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,19 +13,18 @@ const Header = () => {
   return (
     // Header Container
     <header className="mx-auto h-20 flex justify-between py-6 px-4 md:px-20 border-b items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md">
-      
       {/* Logo Container */}
       <div className="logo-container flex h-full space-x-3 items-center">
         {/* Logo for larger screens */}
         <img
-          className="logo hidden md:block w-10"
+          className="hidden md:block w-10"
           src={require("../assets/images/logo.png")}
           alt="Logo"
         />
-        
+
         {/* Logo for smaller screens */}
         <img
-          className="hidden sm:block w-24"
+          className="hidden md:block w-24"
           src={require("../assets/images/zwiggy.png")}
           alt="Zwiggy Logo"
         />
@@ -36,7 +35,7 @@ const Header = () => {
             {/* Menu icon for triggering the sheet */}
             <Menu className="h-6 md:hidden w-6" />
           </SheetTrigger>
-          
+
           {/* Sheet Content with navigation links for mobile */}
           <SheetContent side="left">
             <nav className="flex flex-col items-center gap-4">
@@ -53,7 +52,7 @@ const Header = () => {
           </SheetContent>
         </Sheet>
       </div>
-      
+
       {/* Navigation on desktop */}
       <nav className="md:flex hidden items-center gap-4">
         <Button variant="ghost" className="w-20">
@@ -66,15 +65,15 @@ const Header = () => {
           <Link to="/grocery">Grocery</Link>
         </Button>
       </nav>
-      
+
       {/* Other navigation items */}
       <div className="flex items-center gap-x-6 ">
         {/* Shopping Cart icon */}
         <ShoppingCart className="h-6 w-6" />
-        
+
         {/* Theme Toggle button */}
         <ThemeToggle />
-        
+
         {/* Profile Button */}
         <ProfileButton />
       </div>
